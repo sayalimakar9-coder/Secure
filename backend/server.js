fd6d51126bece570062938234ad50bf2fe48e1e0
walunjt466-beep <walunjt466@gmail.com>
2026-03-04 13:58:37 +0530

    Add x-auth-token to CORS allowed headers

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 }));
 
 app.use(express.json());
